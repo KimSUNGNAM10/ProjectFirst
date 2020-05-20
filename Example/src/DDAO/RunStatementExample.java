c6a5cdbd695e0e343ebc3286b0c92672320f88e9
messi <messi@175.127.147.197>
2020-05-20 23:41:00 +0900

    addd

---
+++ Example/src/DDAO/RunStatementExample.java
@@ -0,0 +1,10 @@
+package DDAO;
+
+public class RunStatementExample {
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 2;
+		int result = x + y;
+		System.out.println(x + "+" + y + "=" + result);
+	}
+}
---
+++ Example/src/DDAO/VariableScopeExample.java
@@ -0,0 +1,7 @@
+package DDAO;
+
+public class VariableScopeExample {
+	public static void main(String[] args) {
+		
+	}
+}
