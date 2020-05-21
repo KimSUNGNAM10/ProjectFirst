0e9da1a0a5329fff165130f27b447b88351c740a
messi <messi@175.127.147.197>
2020-05-21 23:46:53 +0900

    zccc

--- Example/src/DDAO/VariableScopeExample.java
+++ Example/src/DDAO/VariableScopeExample.java
@@ -2,6 +2,11 @@ package DDAO;
 
 public class VariableScopeExample {
 	public static void main(String[] args) {
-		
+		int v1 = 20;
+		if (v1 > 13) {
+			int v2 = v1 - 10;
+			int v3 = v1 + v2 + 5;
+			System.out.println(v3);
+		}
 	}
 }
