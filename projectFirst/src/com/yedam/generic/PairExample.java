a9ca90dfcaa0d2ed035094855152b0c0df951bba
admin <admin@YD03-10>
2020-05-21 12:41:13 +0900

    adddd

---
+++ projectFirst/src/com/yedam/generic/PairExample.java
@@ -0,0 +1,15 @@
+package com.yedam.generic;
+
+public class PairExample {
+	public static void main(String[] args) {
+		Pair<Integer, String> p1 = new Pair<Integer, String>(1, "사과");
+		Pair<Integer, String> p2 = new Pair<>(2, "사과"); //<생략가능>
+		if (Utils.compare(p1, p2))
+			System.out.println("논리적으로 동등한 객체");
+		else
+			System.out.println("논리적으로 다른 객체");
+		
+		int result = Utils.compare(11, 22);
+		System.out.println(result);
+	}
+}
